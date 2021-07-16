35b9e339ad90abd6e891b43b83660da4e63891a9
Subhajit102 <subhajitmitra.1996@gmail.com>
2021-07-17 00:57:07 +0530

    first commit

---
+++ SimpleApi/Controllers/QuotesController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SimpleApi.Models;
+using System.Linq;
+
+namespace SimpleApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        private IConfiguration Configuration;
+
+        public QuotesController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+        [HttpGet]
+        public string Index()
+        {
+            string quote = "";
+            string connString = this.Configuration.GetConnectionString("MyConn");
+            using (SqlConnection con= new SqlConnection(connString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select quote from quotes where id=1", con);
+                SqlDataReader sqlDataReader = cmd.ExecuteReader();
+                while(sqlDataReader.Read())
+                {
+                    quote = sqlDataReader["quote"].ToString();
+                }
+                con.Close();
+            }
+            return quote;
+        }
+
+        [HttpGet]
+        public string getQuotes()
+        {
+            string quote = "";
+            using (var context = new quotesDBContext())
+            {
+                quote = (from q in context.Quotes
+                             where q.Id == 3
+                             select q.Quote1).FirstOrDefault().ToString();
+                
+            }
+            return quote;
+        }
+        [HttpPost]
+        public bool updateQuotes()
+        {
+            using (var context = new quotesDBContext())
+            {
+                var quoteDetails = (from q in context.Quotes
+                                    where q.Id == 4
+                                    select q).FirstOrDefault();
+                quoteDetails.Quote1 = "UPDATED QUOTE";
+                context.SaveChanges();
+
+            }
+            return true;
+        }
+
+    }
+}
---
+++ SimpleApi/Models/Quote.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace SimpleApi.Models
+{
+    public partial class Quote
+    {
+        public int Id { get; set; }
+        public string Quote1 { get; set; }
+    }
+}
